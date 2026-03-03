c6330a01f412a7af5ed856a5d2e3a6b33cad03d4
Nik Pushkarski <11289396+nikpushkarski@users.noreply.github.com>
2026-03-02 21:54:36 -0500

    add the root to compilation

---
+++ dist/assignments/01_Mar2_2026/recreate.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test = "test";
+console.log(test);
+//# sourceMappingURL=recreate.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,24 @@
 "use strict";
+// type Input <T> = T[]
+// interface Output <T> {
+//   timestamp: Date
+//   value: T[]
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-let message = 100;
-console.log(message);
+function label(input) {
+    return {
+        date: new Date(),
+        value: input.data
+    };
+}
+const stringOutput = label({ creatorName: 'Dorothy', data: 'hello' });
+const numberOutput = label({ creatorName: 'Tallulah', data: 100 });
+function formatString(value) {
+    console.log(value.toUpperCase());
+}
+function formatNumber(value) {
+    console.log(value.toFixed(2));
+}
+formatString(stringOutput.value);
+formatNumber(numberOutput.value);
 //# sourceMappingURL=index.js.map
